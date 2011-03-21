bfe7ac871b53c210032a60b7caab4789b52c4928
Sébasten Dolard <sdolard@gmail.com>
2011-03-21 22:46:36 +0100

    renaming

--- web_server_core/private/http_request_p.cpp
+++
@@ -1,8 +0,0 @@
-#include "http_request_p.h"
-
-const QSet<QString> HttpRequestPrivate::REQUEST_HEADER_FIELD_NAMES = QSet<QString>() << "Accept" << "Accept-Charset" << "Accept-Encoding"
-                                                             << "Accept-Language" << "Authorization" << "Expect" << "From" <<
-                                                             "Host" << "If-Match" << "If-Modified-Since" << "If-None-Match" <<
-                                                             "If-Range" << "If-Unmodified-Since" << "Max-Forwards" <<
-                                                             "Proxy-Authorization" << "Range" << "Referer" << "TE" << "User-Agent";
-
